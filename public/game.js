c5f7b74178a081cb8baa411644a7cc4ec8d5c5e5
Jack <johnkinney443@gmail.com>
2026-03-14 00:14:09 -0500

    Updating button styling

--- public/game.js
+++ public/game.js
@@ -59,10 +59,14 @@ function makeSearchHandler(inputEl, acListEl, onPick, debounceMs = 280) {
   });
 }
 
+const VOL_HIGH = `<svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02zM14 3.23v2.06c2.89.86 5 3.54 5 6.71s-2.11 5.85-5 6.71v2.06c4.01-.91 7-4.49 7-8.77s-2.99-7.86-7-8.77z"/></svg>`;
+const VOL_LOW  = `<svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M3 9v6h4l5 5V4L7 9H3zm13.5 3c0-1.77-1.02-3.29-2.5-4.03v8.05c1.48-.73 2.5-2.25 2.5-4.02z"/></svg>`;
+const VOL_MUTE = `<svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor"><path d="M16.5 12c0-1.77-1.02-3.29-2.5-4.03v2.21l2.45 2.45c.03-.2.05-.41.05-.63zm2.5 0c0 .94-.2 1.82-.54 2.64l1.51 1.51C20.63 14.91 21 13.5 21 12c0-4.28-2.99-7.86-7-8.77v2.06c2.89.86 5 3.54 5 6.71zM4.27 3L3 4.27 7.73 9H3v6h4l5 5v-6.73l4.25 4.25c-.67.52-1.42.93-2.25 1.18v2.06c1.38-.31 2.63-.95 3.69-1.81L19.73 21 21 19.73l-9-9L4.27 3zM12 4L9.91 6.09 12 8.18V4z"/></svg>`;
+
 function makeVolumeControls(audioEl, btnEl, sliderEl) {
   let lastVol = 1;
   const setIcon = () => {
-    btnEl.textContent = audioEl.volume === 0 ? "\uD83D\uDD07" : audioEl.volume < 0.5 ? "\uD83D\uDD09" : "\uD83D\uDD0A";
+    btnEl.innerHTML = audioEl.volume === 0 ? VOL_MUTE : audioEl.volume < 0.5 ? VOL_LOW : VOL_HIGH;
   };
   sliderEl.addEventListener("input", () => {
     audioEl.volume = sliderEl.value;
@@ -79,9 +83,12 @@ function makeVolumeControls(audioEl, btnEl, sliderEl) {
   });
 }
 
+const SVG_PLAY  = `<svg width="12" height="12" viewBox="0 0 12 12" fill="currentColor"><path d="M2 1.5l9 4.5-9 4.5z"/></svg>`;
+const SVG_PAUSE = `<svg width="12" height="12" viewBox="0 0 12 12" fill="currentColor"><path d="M3 1h2v10H3zM7 1h2v10H7z"/></svg>`;
+
 function makePreviewPlayer(audioEl, btnEl, fillEl, timeEl) {
   let playing = false, raf = null;
-  btnEl.textContent = "\u25B6";
+  btnEl.innerHTML = SVG_PLAY;
   fillEl.style.width = "0%";
   timeEl.textContent = "0:00 / 0:30";
 
@@ -93,13 +100,13 @@ function makePreviewPlayer(audioEl, btnEl, fillEl, timeEl) {
   }
   function stop() {
     audioEl.pause(); playing = false;
-    btnEl.textContent = "\u25B6"; btnEl.classList.remove("playing");
+    btnEl.innerHTML = SVG_PLAY; btnEl.classList.remove("playing");
     cancelAnimationFrame(raf);
   }
   btnEl.onclick = () => {
     if (playing) { stop(); } else {
       audioEl.currentTime = 0; audioEl.play().catch(() => {});
-      playing = true; btnEl.textContent = "\u23F8"; btnEl.classList.add("playing");
+      playing = true; btnEl.innerHTML = SVG_PAUSE; btnEl.classList.add("playing");
       cancelAnimationFrame(raf); raf = requestAnimationFrame(tick);
     }
   };
